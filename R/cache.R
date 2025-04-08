d540682e1a4c0082aa7c41137d21de78c0c2087c
Bruno Tremblay <bruno@boostao.ca>
2025-04-08 15:30:26 -0400

    more notes

--- R/cache.R
+++ R/cache.R
@@ -110,25 +110,31 @@ cache_get <- function(tbl, cache = TRUE) {
 
 # Locally cache geotiff
 #' @noRd
-cache_tif <- function(rastertbl, layers, VAR = NULL, hull = NULL, cache = TRUE) {
+cache_tif <- function(source, cache = TRUE, hull = .global[["cache"]][["hull"]]) {
   
-  rastertbl <- "normal_composite"
-  layers <- data.table::data.table(var_nm = c(
-    "PPT_01", "PPT_02", "PPT_03", "PPT_04", "PPT_05", "PPT_06", "PPT_07",
-    "PPT_08", "PPT_09", "PPT_10", "PPT_11", "PPT_12", "Tmax_01", "Tmax_02",
-    "Tmax_03", "Tmax_04", "Tmax_05", "Tmax_06", "Tmax_07", "Tmax_08", "Tmax_09",
-    "Tmax_10", "Tmax_11", "Tmax_12", "Tmin_01", "Tmin_02", "Tmin_03", "Tmin_04",
-    "Tmin_05", "Tmin_06", "Tmin_07", "Tmin_08", "Tmin_09", "Tmin_10", "Tmin_11",
-    "Tmin_12", "lr_PPT_01", "lr_PPT_02", "lr_PPT_03", "lr_PPT_04", "lr_PPT_05",
-    "lr_PPT_06", "lr_PPT_07", "lr_PPT_08", "lr_PPT_09", "lr_PPT_10", "lr_PPT_11",
-    "lr_PPT_12", "lr_Tmax_01", "lr_Tmax_02", "lr_Tmax_03", "lr_Tmax_04",
-    "lr_Tmax_05", "lr_Tmax_06", "lr_Tmax_07", "lr_Tmax_08", "lr_Tmax_09",
-    "lr_Tmax_10", "lr_Tmax_11", "lr_Tmax_12", "lr_Tmin_01", "lr_Tmin_02",
-    "lr_Tmin_03", "lr_Tmin_04", "lr_Tmin_05", "lr_Tmin_06", "lr_Tmin_07",
-    "lr_Tmin_08", "lr_Tmin_09", "lr_Tmin_10", "lr_Tmin_11", "lr_Tmin_12",
-    "dem2_WNA"
-  ))
-  layers[, laynum := seq_along(var_nm)]
+  #TODO get source, output SpatRaster
+  
+  source <- list(
+    list(
+      tbl = "normal_composite",
+      layers = data.table::data.table(var_nm = c(
+        "PPT_01", "PPT_02", "PPT_03", "PPT_04", "PPT_05", "PPT_06", "PPT_07",
+        "PPT_08", "PPT_09", "PPT_10", "PPT_11", "PPT_12", "Tmax_01", "Tmax_02",
+        "Tmax_03", "Tmax_04", "Tmax_05", "Tmax_06", "Tmax_07", "Tmax_08", "Tmax_09",
+        "Tmax_10", "Tmax_11", "Tmax_12", "Tmin_01", "Tmin_02", "Tmin_03", "Tmin_04",
+        "Tmin_05", "Tmin_06", "Tmin_07", "Tmin_08", "Tmin_09", "Tmin_10", "Tmin_11",
+        "Tmin_12", "lr_PPT_01", "lr_PPT_02", "lr_PPT_03", "lr_PPT_04", "lr_PPT_05",
+        "lr_PPT_06", "lr_PPT_07", "lr_PPT_08", "lr_PPT_09", "lr_PPT_10", "lr_PPT_11",
+        "lr_PPT_12", "lr_Tmax_01", "lr_Tmax_02", "lr_Tmax_03", "lr_Tmax_04",
+        "lr_Tmax_05", "lr_Tmax_06", "lr_Tmax_07", "lr_Tmax_08", "lr_Tmax_09",
+        "lr_Tmax_10", "lr_Tmax_11", "lr_Tmax_12", "lr_Tmin_01", "lr_Tmin_02",
+        "lr_Tmin_03", "lr_Tmin_04", "lr_Tmin_05", "lr_Tmin_06", "lr_Tmin_07",
+        "lr_Tmin_08", "lr_Tmin_09", "lr_Tmin_10", "lr_Tmin_11", "lr_Tmin_12",
+        "dem2_WNA"
+      ), laynum = 1:73)
+    )
+  )
+  
   xyz <- data.frame(
     lon = runif(500000, -127, -93),
     lat = runif(500000, 52, 57)
@@ -155,6 +161,22 @@ cache_tif <- function(rastertbl, layers, VAR = NULL, hull = NULL, cache = TRUE) 
   a <- terra::relate(terra::vect(info$hull), terra::vect(xyz, crs = "EPSG:4326"), "intersects", pairs = TRUE, na.rm = TRUE)
   
   rid <- info[a[,1] |> unique() |> sort(), rid]
+  
+  prof <- .globals$sessprof$get()
+  
+  # Translate hangs over 10k bands.
+  sf::gdal_utils("translate", 
+                 source = "PG:host=%s port=%s dbname=%s user=%s password=%s schema=public table='%s' column=rast mode=2 where='rid IN (%s)'" |>
+                   sprintf(prof$host, prof$port, prof$dbname, prof$user, prof$password, rastertbl, paste0(rid, collapse = ",")), 
+                 destination = "%s.tif" |> sprintf(rastertbl), 
+                 options = c("-of", "GTiff", "-ot", "Float32", "-co", "COMPRESS=LZW"), 
+                 quiet = TRUE)
+  
+  sf::gdal_utils("warp", 
+                 source = c("normal_composite_1_nontiled.tif", "normal_composite_2_nontiled.tif"), 
+                 destination = "combined_non_tiled.tif", 
+                 options = c("-of", "GTiff", "-ot", "Float32", "-co", "COMPRESS=LZW"), 
+                 quiet = TRUE)
   
   
 }
